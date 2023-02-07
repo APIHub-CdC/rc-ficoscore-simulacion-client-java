943bf46de63a3fcf82480bb6bfe4f6bab685e23b
Hector Manuel Ramirez Ramirez <hmramirezr@MacBook-Pro-Hector-Manuel-Ramirez-Ramirez.local>
2023-02-07 10:43:27 -0600

    Removing segmented reports

--- src/test/java/com/cdc/apihub/mx/RC/FicoScore/simulacion/api/ReporteDeCreditoConFicoScoreApiTest.java
+++ src/test/java/com/cdc/apihub/mx/RC/FicoScore/simulacion/api/ReporteDeCreditoConFicoScoreApiTest.java
@@ -9,15 +9,9 @@ import org.slf4j.LoggerFactory;
 import com.cdc.apihub.mx.RC.FicoScore.simulacion.client.ApiClient;
 import com.cdc.apihub.mx.RC.FicoScore.simulacion.client.ApiException;
 import com.cdc.apihub.mx.RC.FicoScore.simulacion.model.CatalogoEstados;
-import com.cdc.apihub.mx.RC.FicoScore.simulacion.model.Consultas;
-import com.cdc.apihub.mx.RC.FicoScore.simulacion.model.Creditos;
 import com.cdc.apihub.mx.RC.FicoScore.simulacion.model.DomicilioPeticion;
-import com.cdc.apihub.mx.RC.FicoScore.simulacion.model.DomiciliosRespuesta;
-import com.cdc.apihub.mx.RC.FicoScore.simulacion.model.Empleos;
-import com.cdc.apihub.mx.RC.FicoScore.simulacion.model.Mensajes;
 import com.cdc.apihub.mx.RC.FicoScore.simulacion.model.PersonaPeticion;
 import com.cdc.apihub.mx.RC.FicoScore.simulacion.model.Respuesta;
-import com.cdc.apihub.mx.RC.FicoScore.simulacion.model.Scores;
 
 
 public class ReporteDeCreditoConFicoScoreApiTest {
@@ -35,7 +29,6 @@ public class ReporteDeCreditoConFicoScoreApiTest {
     public void getReporteTest() throws ApiException {
     	
         String xApiKey = "your_apikey";
-        String xFullReport = null;
         
         PersonaPeticion request = new PersonaPeticion();
         DomicilioPeticion domicilio = new DomicilioPeticion();
@@ -56,41 +49,11 @@ public class ReporteDeCreditoConFicoScoreApiTest {
         
         request.setDomicilio(domicilio);
         
-        Respuesta response = api.getReporte(xApiKey, request, xFullReport);
+        Respuesta response = api.getReporte(xApiKey, request);
         
         Assert.assertTrue(response.getFolioConsulta() != null);
         
-        	logger.info(response.toString());
-        
-        
-        if (response.getFolioConsulta() != null && (xFullReport == null ||  xFullReport.equals("false") || xFullReport.equals("FALSE"))) {
-        	
-			String folioConsulta = response.getFolioConsulta();
-
-			Consultas consultas = api.getConsultas(folioConsulta, xApiKey);
-			logger.info(consultas.toString());
-			Assert.assertTrue(consultas.getConsultas() != null);
-
-			Creditos creditos = api.getCreditos(folioConsulta, xApiKey);
-			logger.info(creditos.toString());
-			Assert.assertTrue(creditos.getCreditos() != null);
-
-			DomiciliosRespuesta domicilios = api.getDomicilios(folioConsulta, xApiKey);
-			logger.info(domicilios.toString());
-			Assert.assertTrue(domicilios.getDomicilios() != null);
-
-			Empleos empleos = api.getEmpleos(folioConsulta, xApiKey);
-			logger.info(empleos.toString());
-			Assert.assertTrue(empleos.getEmpleos() != null);
-
-			Scores scores = api.getScores(folioConsulta, xApiKey);
-			logger.info(scores.toString());
-			Assert.assertTrue(scores.getScores() != null);
-		
-			Mensajes mensajes = api.getMensajes(folioConsulta, xApiKey);
-			logger.info(mensajes.toString());
-			Assert.assertTrue(mensajes.getMensajes() != null);
-		}
+        logger.info(response.toString());
         
     }
     
